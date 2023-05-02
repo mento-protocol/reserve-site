1ebec59148524a7e8b8cfa94e5f702b50fcf06a2
boqdan <bogdan@bowd.io>
2023-05-02 11:17:14 +0300

    fix: refactor use of constants

--- src/helpers/StakedCeloProvider.ts
+++ src/helpers/StakedCeloProvider.ts
@@ -1,16 +1,8 @@
 import { JsonRpcProvider } from "@ethersproject/providers"
 import BigNumber from "bignumber.js"
 import { ethers } from "ethers"
-
-const STAKED_CELO_MANAGER_ADDRESS = "0x0239b96D10a434a56CC9E09383077A0490cF9398"
-const STAKED_CELO_ERC20_ADDRESS = "0xC668583dcbDc9ae6FA3CE46462758188adfdfC24"
-
-const STAKED_CELO_ERC20_ABI = [
-  "function balanceOf(address account) external view returns (uint256)",
-  "function decimals() external view returns (uint256)",
-]
-
-const STAKED_CELO_MANAGER_ABI = ["function toCelo(uint256 amount) external view returns (uint256)"]
+import { STAKED_CELO_ERC20_ADDRESS, STAKED_CELO_MANAGER_ADDRESS } from "src/contract-addresses"
+import { ERC20_ABI, STAKED_CELO_MANAGER_ABI } from "src/constants/abis"
 
 export class StakedCeloProvider {
   private static _instance: StakedCeloProvider
@@ -19,11 +11,7 @@ export class StakedCeloProvider {
 
   constructor() {
     const provider = new JsonRpcProvider(process.env.CELO_NODE_RPC_URL)
-    this.stakedCelo = new ethers.Contract(
-      STAKED_CELO_ERC20_ADDRESS,
-      STAKED_CELO_ERC20_ABI,
-      provider
-    )
+    this.stakedCelo = new ethers.Contract(STAKED_CELO_ERC20_ADDRESS, ERC20_ABI, provider)
     this.stakedCeloManager = new ethers.Contract(
       STAKED_CELO_MANAGER_ADDRESS,
       STAKED_CELO_MANAGER_ABI,
