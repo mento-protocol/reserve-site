684e24d1aa7581a82b6eeb517029b404fcfbe0c5
boqdan <bogdan@bowd.io>
2023-11-30 12:41:43 +0100

    fix: refactor function

--- src/providers/utils.ts
+++ src/providers/utils.ts
@@ -1,9 +1,5 @@
 import BigNumber from "bignumber.js"
 
-export function formatNumber(value, decimals?: number) {
-  if (decimals) {
-    return new BigNumber(value).dividedBy(new BigNumber(10).pow(decimals)).toNumber()
-  } else {
-    return new BigNumber(value).dividedBy(1_000_000_000_000_000_000).toNumber()
-  }
+export function formatNumber(value: string | number, decimals = 18) {
+  return new BigNumber(value).dividedBy(new BigNumber(10).pow(decimals)).toNumber()
 }
