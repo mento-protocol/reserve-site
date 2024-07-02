d2f42e532c15b2d4b4daee6f8af0bb4cf7c32cec
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2024-07-02 14:11:16 +0200

    feat (amount component) : add loading skeleton

--- src/components/Amount.tsx
+++ src/components/Amount.tsx
@@ -1,5 +1,6 @@
 import { CardBackground } from "@/components/CardBackground";
 import { cn } from "@/styles/helpers";
+import { Skeleton } from "./TextSkeleton";
 interface AmountProps {
   label: string;
   units: number;
@@ -26,6 +27,10 @@ export default function Amount({
 
   const id = `a-${dasherize(label)}`;
 
+  if (loading) {
+    return <AmountSkeleton />;
+  }
+
   return (
     <CardBackground className="min-w-[220px] px-6 py-4">
       <div className="flex flex-row items-center">
@@ -39,7 +44,7 @@ export default function Amount({
           />
         )}
         <p id={id} className="mb-0 items-end text-left text-[22px] font-medium">
-          <abbr className="cursor-help no-underline font-fg" title={context}>
+          <abbr className="cursor-help font-fg no-underline" title={context}>
             {label}
           </abbr>
         </p>
@@ -48,17 +53,14 @@ export default function Amount({
       <span
         aria-labelledby={id}
         className={cn(
-          "my-6 block text-left text-[32px] font-medium opacity-100 transition-opacity duration-500 font-fg",
-          !loading && value ? "" : "",
-          loading && "amountLoadingStyle",
+          "my-6 block text-left font-fg text-[32px] font-medium opacity-100 transition-opacity duration-500",
         )}
       >
         {display}
       </span>
       <DollarDisplay
-        className="block text-left text-[22px] opacity-100 transition-opacity duration-500 font-fg"
+        className="block text-left font-fg text-[22px] opacity-100 transition-opacity duration-500"
         value={value}
-        loading={loading}
         label={label}
       />
     </CardBackground>
@@ -67,29 +69,19 @@ export default function Amount({
 
 interface DollarDisplayProps {
   value: number;
-  loading: boolean;
   label: string;
   className?: string;
 }
 
-export function DollarDisplay({
-  value,
-  loading,
-  label,
-  className,
-}: DollarDisplayProps) {
+export function DollarDisplay({ value, label, className }: DollarDisplayProps) {
   const displayValue = value && Math.round(value).toLocaleString();
 
   return (
     <span
-      className={cn(
-        className,
-        loading && "animate-loading",
-        "text-[22px] text-mento-blue",
-      )}
+      className={cn(className, "text-[22px] text-mento-blue")}
       aria-label={`Value of ${label} in USD`}
     >
-      {loading ? " " : !!value && `$${displayValue}`}
+      {`$${displayValue}`}
     </span>
   );
 }
@@ -98,12 +90,15 @@ function dasherize(str: string) {
   return str.toLowerCase().split(" ").join("-");
 }
 
-// const secondaryNumberStyle = css({
-//   ["@media (min-width: 597px) and (max-width: 699px)"]: {
-//     display: "none",
-//   },
-// });
-
-// cn(
-//   "block pl-[2em] text-left text-[36px] opacity-100 transition-opacity duration-500 tablet:mb-[8px] phablet:hidden tablet:flex-1 tablet:text-right tablet:text-[28px]",
-// );
+const AmountSkeleton = () => {
+  return (
+    <CardBackground className="flex min-w-[220px] flex-col gap-6 px-6 py-4">
+      <div className="flex flex-row items-center gap-2">
+        <Skeleton className="h-[32px] w-[32px] rounded-full bg-black/10 " />
+        <Skeleton className="h-[22px] w-[65px]  bg-black/10" />
+      </div>
+      <Skeleton className="h-[22px] w-[198px] bg-black/10" />
+      <Skeleton className="h-[22px] w-[105px] bg-black/10" />
+    </CardBackground>
+  );
+};
