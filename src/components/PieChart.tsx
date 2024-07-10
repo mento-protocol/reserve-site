f1554a136a301d4c4e03d64589f7888d68e38d7d
Ryan Noble <ryanjnoble@gmail.com>
2024-07-10 18:49:30 +0200

    Feat(76,79):  Collateral composition (#93)

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -1,13 +1,9 @@
-import { Fragment } from "react";
+import { useMemo } from "react";
 import colors from "src/components/colors";
-import { cn } from "src/styles/helpers";
+import { Chart, ArcElement, ChartData } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 
-enum KeyAsHuman {
-  "stable-value" = "Stable Value Assets*",
-  "natural-capital" = "Natural Capital Backed Assets**",
-}
-
-enum TokenColor {
+export enum TokenColor {
   BTC = colors.orange,
   ETH = colors.purpleGray,
   CELO = colors.gold,
@@ -25,130 +21,42 @@ enum TokenColor {
 }
 
 interface Props {
-  slices: ChartData[];
-  label: string;
-  showFinePrint?: boolean;
+  slices: SliceData[];
   isLoading?: boolean;
 }
 
-const RADIUS = 10;
-const CIRCUMFERENCE = Math.PI * 2 * RADIUS;
-
-export default function PieChart({
-  slices,
-  label,
-  showFinePrint,
-  isLoading,
-}: Props) {
-  const dataWithOffsets = slices.map((data, index) => {
-    let offset = 0;
-    let i = index - 1;
-
-    while (i >= 0) {
-      offset = offset + (slices[i].percent || 0);
-      --i;
-    }
-    return { offset, ...data };
-  });
-
-  return (
-    <figure className="m-0 flex w-[670px] max-w-full flex-wrap pt-[48px]">
-      <figcaption className="min-w-[260px] flex-[3]">
-        <h3 className="mb-[24px] block w-full text-[28px] leading-[48px]">
-          {label}
-        </h3>
-        {slices.map(({ token, percent }) => (
-          <ChartKey
-            key={token}
-            token={token}
-            percent={isLoading ? NaN : percent}
-          />
-        ))}
-        {showFinePrint && (
-          <>
-            <br />
-            <small>
-              Target allocation is dynamic and a function of cStables in
-              circulation.{" "}
-              <a
-                href="https://celo.stake.id/#/proposal/62"
-                target="_blank"
-                rel="noreferrer"
-              >
-                More info
-              </a>
-            </small>
-          </>
-        )}
-      </figcaption>
-      <div
-        className={cn(
-          "flex min-w-[250px] flex-[3]",
-          isLoading && "animate-loading",
-        )}
-      >
-        <svg
-          viewBox="-20 -20 40 40"
-          transform="rotate(-90)"
-          width="100%"
-          height="100%"
-        >
-          {dataWithOffsets.map(({ percent, offset, token }) => {
-            const displayedPercent = percent < 0.1 ? 0.1 : percent;
-            return (
-              <Fragment key={token}>
-                <circle
-                  cx="0"
-                  cy="0"
-                  opacity={0.8}
-                  r={RADIUS}
-                  fill="transparent"
-                  stroke={isLoading ? colors.gray : TokenColor[token]}
-                  strokeWidth={RADIUS - 1}
-                  strokeDasharray={`${CIRCUMFERENCE * (displayedPercent / 100)} ${
-                    CIRCUMFERENCE * (1 - displayedPercent / 100)
-                  }`}
-                  transform={`rotate(${(offset * 360) / 100})`}
-                />
-                <line
-                  x1="0"
-                  x2={RADIUS + 1}
-                  y1="0"
-                  y2={RADIUS + 1}
-                  stroke="white"
-                  strokeWidth="0.1"
-                  transform={`rotate(${((offset - 12.5) * 360) / 100})`}
-                />
-              </Fragment>
-            );
-          })}
-        </svg>
-      </div>
-    </figure>
+export default function PieChart({ slices, isLoading }: Props) {
+  Chart.register(ArcElement);
+  // Inconsistent spacing due to high numbers & borders,
+  // so injection of pseudo slices was done here with flat maps
+  const data: ChartData<"doughnut", number[], string> = useMemo(
+    () => ({
+      labels: slices.map((i) => i.token),
+      datasets: [
+        {
+          label: "Current reserve composition",
+          data: slices.flatMap((i) => [1, i.percent]),
+          backgroundColor: slices.flatMap((i) => [
+            "transparent",
+            TokenColor[i.token],
+          ]),
+          hoverOffset: 0,
+          borderRadius: 10,
+          offset: 0,
+          spacing: 0,
+          borderAlign: "center",
+          borderWidth: slices.flatMap(() => [0, 0.5]),
+          borderColor: "rgba(0,0,0,0.6)",
+        },
+      ],
+    }),
+    [slices],
   );
+
+  return <Doughnut data={data} />;
 }
 
-export interface ChartData {
+export interface SliceData {
   token: string;
   percent: number;
-}
-
-const formatter = new Intl.NumberFormat(undefined, {
-  style: "decimal",
-  maximumFractionDigits: 2,
-});
-
-function ChartKey({ token, percent }: ChartData) {
-  return (
-    <div className="mb-[10px] flex text-[20px]">
-      <div
-        className="h-[20px] w-[20px] rounded-[3px]"
-        style={{ backgroundColor: TokenColor[token] }}
-      />
-      <span className="pl-[10px] pr-[8px] font-bold">
-        {isNaN(percent) ? "??" : formatter.format(percent)}%
-      </span>
-      <span>{KeyAsHuman[token] || token}</span>
-    </div>
-  );
 }
--- src/components/ReserveComposition.tsx
+++ src/components/ReserveComposition.tsx
@@ -1,7 +1,7 @@
-import PieChart, { ChartData } from "@/components/PieChart";
+import { CardBackground } from "@/components/CardBackground";
+import PieChart, { SliceData, TokenColor } from "@/components/PieChart";
 import useHoldings from "@/hooks/useHoldings";
 import { HoldingsApi } from "@/service/holdings";
-import { Head } from "next/document";
 
 export function sumCeloTotal(holdings: HoldingsApi) {
   const { custody, frozen, unfrozen } = holdings.celo;
@@ -12,12 +12,12 @@ export function sumNonCelo({ otherAssets }: HoldingsApi) {
   return otherAssets.reduce((prev, current) => current.value + prev, 0);
 }
 
-function getPercents(holdings: HoldingsApi): ChartData[] {
+function getPercents(holdings: HoldingsApi): SliceData[] {
   const celoTotal = sumCeloTotal(holdings);
   const total = celoTotal + sumNonCelo(holdings);
 
   function toPercent(value: number) {
-    return (value / total) * 100;
+    return (value * 100) / total;
   }
 
   return [{ token: "CELO", percent: toPercent(celoTotal) }].concat(
@@ -33,47 +33,47 @@ function getPercents(holdings: HoldingsApi): ChartData[] {
   );
 }
 
-function findOldestValueUpdatedAt(data?: HoldingsApi): number {
-  if (!data) {
-    return 0;
-  }
-
-  return Math.min(
-    ...data.otherAssets
-      .map((token) => token.updated)
-      .concat([
-        data.celo.custody.updated,
-        data.celo.frozen.updated,
-        data.celo.unfrozen.updated,
-      ]),
-  );
-}
-
 export const ReserveComposition = () => {
   const { data, isLoadingCelo, isLoadingOther } = useHoldings();
   const percentages = getPercents(data);
 
   return (
-    <article>
-      <Head>
-        <link
-          rel="preload"
-          href="/api/holdings/celo"
-          as="fetch"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/api/holdings/other"
-          as="fetch"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <PieChart
-        label={"Current Composition"}
-        slices={percentages}
-        isLoading={isLoadingCelo || isLoadingOther}
-      />
-    </article>
+    <>
+      <h2 className="mb-4 mt-8 block text-center font-fg text-[32px] font-medium lg:hidden">
+        Current reserve composition
+      </h2>
+      <CardBackground className="mt-0 p-10 lg:mt-14">
+        <article>
+          <h2 className="mb-8 hidden text-center font-fg text-[32px] font-medium lg:block">
+            Current reserve composition
+          </h2>
+          <section className="flex flex-col-reverse items-center justify-center md:flex-row">
+            <div className="grid grid-cols-2 gap-x-16 gap-y-6">
+              {percentages.map((item) => (
+                <div
+                  key={item.token}
+                  className="flex flex-row items-center justify-start"
+                >
+                  <div
+                    className="mr-[10px] h-[18px] w-[18px] rounded"
+                    style={{ backgroundColor: TokenColor[item.token] }}
+                  ></div>
+                  <div>
+                    <span className="font-medium">{`${item.percent.toFixed(2)}%`}</span>{" "}
+                    {item.token}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="mb-8 ml-0 max-h-[300px] max-w-[300px] md:mb-0 md:ml-[64px] lg:ml-[128px]">
+              <PieChart
+                slices={percentages}
+                isLoading={isLoadingCelo || isLoadingOther}
+              />
+            </div>
+          </section>
+        </article>
+      </CardBackground>
+    </>
   );
 };
