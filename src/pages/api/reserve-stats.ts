0e37b185154452885b33b2e3873a509678410827
Bayological <6872903+bayological@users.noreply.github.com>
2024-12-03 18:12:29 -0500

    feat: fetch ratio from api

---
+++ src/pages/api/reserve-stats.ts
@@ -0,0 +1,31 @@
+import * as Sentry from "@sentry/nextjs";
+import { NextApiRequest, NextApiResponse } from "next";
+
+interface ReserveTotalsResponse {
+  collateralization_ratio: number;
+  total_reserve_value_usd: number;
+  total_outstanding_stables_usd: number;
+}
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method === "GET") {
+      const start = Date.now();
+
+      const response = await fetch(
+        "https://mento-analytics-api-12390052758.us-central1.run.app/api/v1/reserve/stats",
+      );
+      const result: ReserveTotalsResponse = await response.json();
+
+      res.setHeader("Server-Timing", `ms;dur=${Date.now() - start}`);
+      res.json(result);
+    } else {
+      res.status(405);
+    }
+  } catch (error) {
+    Sentry.captureException(error);
+    res
+      .status(error.statusCode || 500)
+      .json({ message: error.message || "unknownError" });
+  }
+}
